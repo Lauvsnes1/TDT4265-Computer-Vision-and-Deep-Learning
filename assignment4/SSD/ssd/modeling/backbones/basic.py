316d62222924b0c0031c3d91173b4d413dbc6d5e
nilsmh <nmhillesund99@gmail.com>
2022-03-15 11:32:43 +0100

    started on task4

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -1,4 +1,5 @@
-import torch
+# import torch 
+from torch import nn
 from typing import Tuple, List
 
 
@@ -13,6 +14,12 @@ class BasicModel(torch.nn.Module):
      shape(-1, output_channels[3], 3, 3),
      shape(-1, output_channels[4], 1, 1)]
     """
+
+    # Each convolutional layer has a filter size of 3 × 3 and padding of 1. 
+    # Each MaxPool2D layer has a stride of 2 and a kernel size of 2 × 2. 
+    # Note that output channels[i] is a variable that you can set in the config file (By default this is: [128, 256, 128, 128, 64, 64]). 
+    # The last convolution should have a stride of 1, padding of 0, and a kernel size of 3 × 3
+
     def __init__(self,
             output_channels: List[int],
             image_channels: int,
@@ -20,6 +27,50 @@ class BasicModel(torch.nn.Module):
         super().__init__()
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
+
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(in_channels=image_channels, out_channels=32, kernel_size=3, stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1,padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Conv2d(in_channels=64, out_channels=output_channels[0], kernel_size=3, stride=2,padding=1),
+            nn.ReLU(),
+        ), nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=output_channels[0], output_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, output_channels=output_channels[1], kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        ), nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=output_channels[1], output_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, output_channels=output_channels[2], kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        ), nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=output_channels[2], output_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, output_channels=output_channels[3], kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        ), nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=output_channels[3], output_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, output_channels=output_channels[4], kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
+        ), nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=output_channels[4], output_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, output_channels=output_channels[5], kernel_size=3, stride=1, padding=0),
+            nn.ReLU()
+        )
 
     def forward(self, x):
         """
@@ -34,7 +85,7 @@ class BasicModel(torch.nn.Module):
         where out_features[0] should have the shape:
             shape(-1, output_channels[0], 38, 38),
         """
-        out_features = []
+        out_features = [self.feature_extractor]
         for idx, feature in enumerate(out_features):
             out_channel = self.out_channels[idx]
             h, w = self.output_feature_shape[idx]
